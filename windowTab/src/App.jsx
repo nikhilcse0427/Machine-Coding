8adfd2435d7c83bf757278bc0bf63e35a447b718
Nikhil Verma <155971659+nikhilcse0427@users.noreply.github.com>
2026-03-10 12:45:45 +0000

    window tab machine code done

--- windowTab/src/App.jsx
+++ windowTab/src/App.jsx
@@ -1,35 +1,47 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const tabs = [
+    {
+      label: "dashboard",
+      content: "This tab contains dashboard page",
+    },
+    {
+      label: "payment",
+      content: "This tab contain payment page",
+    },
+    {
+      label: "contact",
+      content: "This tab contains contact page",
+    },
+    {
+      label: "profile",
+      content: "This tab contains profile page",
+    },
+  ];
 
+  const [tabNum, setTabNum] = useState(1);
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="container">
+        <div className="tabBox">
+          {tabs.map((tab, id) => (
+            <>
+              <button onClick={() => setTabNum(id)} key={id}>
+                {tab.label}
+              </button>
+            </>
+          ))}
+        </div>
+        {tabs.map(
+          (tab, id) => id === tabNum && <h1 key={id}>{tab.content}</h1>
+        )}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
